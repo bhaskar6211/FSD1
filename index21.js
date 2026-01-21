0c4b00bb67aa2a851e680ab988ae00e2724189da
bhaskar6211 <bhaskarsah63@gmail.com>
2026-01-21 23:39:16 +0530

    Day-21/1/2026

--- index21.js
+++ index21.js
@@ -1,5 +1,5 @@
 // calculator arrow function in js
-const calculator = (a, b, operation) => {
+/*const calculator = (a, b, operation) => {
     switch (operation) {
         case 'add':
             return a + b;
@@ -18,7 +18,44 @@ const calculator = (a, b, operation) => {
             return 'Error: Invalid operation';
     }    
 }
-let a=prompt("Enter first number:");
-let b=prompt("Enter second number:");
+let a=parseInt(prompt("Enter first number:"));
+let b=parseInt(prompt("Enter second number:"));
 let operation=prompt("Enter operation: add, subtract, multiply, divide: ");
-document.writeln(calculator(a, b, operation));
+document.writeln(calculator(a, b, operation));*/
+
+//make a object using arrow function
+/*const Person = (name, age, city) => {
+    return {
+        student_name: name,
+        age: age,
+        city: city
+    };
+};
+const p1 = Person("Bhaskar Sah", 20, "Janakpur");
+document.writeln(`Name: ${p1.student_name}, Age: ${p1.age}, City: ${p1.city}<br>`);
+
+const p2 = Person("Dikshya Tiwari", 21, "Kathmandu");
+document.writeln(`Name: ${p2.student_name}, Age: ${p2.age}, City: ${p2.city}`);
+*/
+
+
+const student = [
+    {
+        id: 1,
+        name: "Bhaskar Sah",
+        age: 20
+    },
+    {
+        id: 2,
+        name: "Manish Gautam",
+        age: 21
+    },
+    {
+        id: 3,
+        name: "Sujan Dahal",
+        age: 21
+    }
+];
+student.forEach((s) => {
+    document.writeln(`ID: ${s.id}, Name: ${s.name}, Age: ${s.age}<br><br>`);
+});
---
+++ new.js
@@ -0,0 +1,4 @@
+const calculator = (a, b) => {
+    document.writeln(a - b);
+};
+calculator(5, 10);
